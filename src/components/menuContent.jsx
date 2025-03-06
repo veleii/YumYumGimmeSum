4d6e2cae0d67a868f80f19d252e62ea81d72cfa4
veleii <vendelanorinder@hotmail.se>
2025-03-06 15:52:29 +0100

    update

--- src/components/menuContent.jsx
+++ src/components/menuContent.jsx
@@ -2,7 +2,19 @@ export default function menuContent() {
     return (
         <div className="menu_content_container">
             <img src="/src/assets/menu-background.png" alt="leaf plural" className="main_img"/>
-            <div className="menu_overlay"></div>
+            <div className="menu_overlay">
+                <header>
+                    
+                </header>
+                <h1>MENY</h1>
+                <section>
+                {/*     <h2></h2>
+                    <div className="menu_dotted_line"></div>
+                    <section className="sum">
+							<h4>{item.price}kr</h4>
+						</section> */}
+                </section>
+            </div>
         </div>
     );
 }
