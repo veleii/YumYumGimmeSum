1384781c10a6e07325b5855395be092e64067e56
veleii <vendelanorinder@hotmail.se>
2025-03-07 16:18:36 +0100

    nu tar jag fredag

--- src/components/cartContent.jsx
+++ src/components/cartContent.jsx
@@ -1,9 +1,8 @@
 export default function cartContent() {
     return (
-        <>
+        <article>
             <div className="cart_overlay">
-               
-
+            
                 <section className="fetch_city">
                      <h2>KARLSTAD</h2>
                      <p>kantarell, scharlottenlök, morot, bladpersilja</p>
@@ -13,15 +12,15 @@ export default function cartContent() {
 							<h4>9kr</h4>
 						</section> 
                 </section>
-                
             </div>
+         
             <section className="cart_footer">
                     <section className="total_footer">
                         <p className="sum_footer_left">TOTALT</p> <p className="sum_footer_right">101 SEK</p>
                     </section>
                     <button className="button_footer">TAKE MY MONEY!</button>
                 </section>
-            </>
+            </article>
     );
 }
 
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -9,7 +9,6 @@ export default function Cart() {
         <div className="cart">
             <Header />
 
-            
             {cartContent()}  
         </div>
     );
